0985c712abceecdb9d5efb8da6c391e14597f86d
Nikita-200 <nikitajohnyk@gmail.com>
2023-06-05 10:24:55 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [message, setMessage] = useState('');
+
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      try {
+        const response = await axios.post('/api/upload', formData);
+        setMessage(response.data.message);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>React Frontend</h1>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
